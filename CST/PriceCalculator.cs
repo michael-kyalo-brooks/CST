85bea5374412db5ba1dfefb6c38c57125f1f0509
Michael Brooks <michael.kyalo.brooks@gmail.com>
2019-07-29 12:30:16 +0100

    Second Test passed

---
+++ CST/PriceCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CST
+{
+    public class PriceCalculator : IPriceCalculator
+    {
+        public decimal Calculate(IEnumerable<string> list)
+        {
+            if (list.Count() > 0)
+            {
+                return 0.6m;
+            }
+
+            return 0m;
+        }
+    }
+}
